fb763c8886c016eedbcc63e4a6caa2d9b7779553
igame3dbill <bill@igame3d.com>
2018-04-12 19:12:20 -0600

    Unity Text Adventure Tutorial Assets This is broken, see read me for details.

---
+++ Assets/Scripts/RoomNavigation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNavigation : MonoBehaviour {
+
+    public Room currentRoom;
+    Dictionary<string, Room> exitDictionary = new Dictionary<string, Room>();
+
+    GameController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<GameController>();
+
+    }
+
+    public void UnpackExitsInRoom()
+    {
+        for (int i = 0; i < currentRoom.exits.Length; i++)
+        {
+            exitDictionary.Add(currentRoom.exits[i].keyString, currentRoom.exits[i].valueRoom);
+
+            controller.interactionDescriptionsInRoom.Add(currentRoom.exits[i].exitDescription + " " + currentRoom.exits[i].keyString.ToUpper());
+        }
+    }
+    public void AttemptToChangeRooms(string directionNoun)
+    {
+        if (exitDictionary.ContainsKey(directionNoun))
+        {
+            currentRoom = exitDictionary[directionNoun];
+            controller.LogStringWithReturn("You move to the " + directionNoun);
+            controller.DisplayRoomText();
+        }
+        else
+        {
+            controller.LogStringWithReturn("You can't go " + directionNoun);
+        }    
+    }
+
+    public void ClearExits()
+    {
+        exitDictionary.Clear();
+    }
+
+}
